297765d6baf2f01291ba3c859273bc09e4628020
chschan <chschan@gmail.com>
2016-10-07 12:05:46 +1100

    Fixed RemoveRowsAndOrColumns to handle spaces and capitalisation

--- R/table.R
+++ R/table.R
@@ -2,12 +2,26 @@
 #' \code{RemoveRowsAndOrColumns}
 #' @description Removes rows or columns from the table.
 #' @param x The data that is being analyzed
-#' @param row.names.to.remove A vector of the row labels to remove.
-#' @param column.names.to.remove A vector of the column labels to remove.
+#' @param row.names.to.remove A vector or comma-separated string containing the row labels to remove, if row names are defined. Trailing spaces are removed and lower/upper case is ignored
+#' @param column.names.to.remove A vector or comma-separated string containing the column labels to remove, if column names are defined
 #' @export
 RemoveRowsAndOrColumns <- function(x,
                                    row.names.to.remove = c("NET", "Total", "SUM"),
                                    column.names.to.remove = c("NET", "Total", "SUM"))
 {
-    x[!rownames(x) %in% row.names.to.remove, !colnames(x) %in% column.names.to.remove, drop = FALSE]
+    ind <- list(1:nrow(x), 1:ncol(x))
+    for (i in 1:2)
+    {
+        tmpname <- switch(i, rownames(x), colnames(x))
+        tmpstring <- switch(i, row.names.to.remove, column.names.to.remove)
+
+        if (!is.null(tmpname) && !is.null(tmpstring))
+        {
+            tmpname <- tolower(trimws(tmpname))
+            tmpstring <- unlist(strsplit(tmpstring, split=","))
+            tmpstring <- tolower(trimws(tmpstring))
+            ind[[i]] <- which(!tmpname %in% tmpstring)
+        }
+    }
+    x[ind[[1]], ind[[2]], drop = FALSE]
 }
--- tests/testthat/test-table.R
+++ tests/testthat/test-table.R
@@ -6,3 +6,38 @@ test_that("RemoveRowsAndOrColumns works",
               x <- RemoveRowsAndOrColumns(x, "A", c("C","A"))
               expect_equal(prod(dim(x)), 2)
           })
+
+dat <- structure(c(39.9370629370629, 45.9459459459459, 43.1311475409836,
+54.2222222222222, 43.7954545454545, 42, 40.2748538011696, 44.6754966887417,
+42.3385093167702), .Dim = c(9L, 1L), .Dimnames = list(c("Coca Cola ",
+"Diet Coke", "Coke Zero", "Pepsi Light ", "Pepsi Max", "Pepsi ",
+"NET Sugarred", "NET Sugarless", "NET"), "Age in years"), statistic = "Average", name = "Q3. Age in years by Preferred cola", questions = c("Q3. Age in years",
+"Preferred cola"))
+
+test_that("RemoveRowsAndOrColumns handles NULL",
+          {
+              d2 <- RemoveRowsAndOrColumns(dat, row.names.to.remove = NULL)
+              expect_equal(nrow(d2), 9)
+          })
+
+test_that("RemoveRowsAndOrColumns handles spaces in middle",
+          {
+              d3 <- RemoveRowsAndOrColumns(dat, row.names.to.remove = "NET Sugarless,NET Sugarred,   Net")
+              expect_equal(nrow(d3), 6)
+          })
+
+test_that("RemoveRowsAndOrColumns handles trailing spaces",
+          {
+              d4 <- RemoveRowsAndOrColumns(dat, row.names.to.remove = "Pepsi")
+              expect_equal(nrow(d4), 8)
+          })
+
+test_that("RemoveRowAndOrColumns ignores lower/upper case",
+          {
+              d5 <- RemoveRowsAndOrColumns(dat, row.names.to.remove = "diet coke, coke zero")
+              expect_equal(nrow(d5), 7)
+          })
+
+
+
+
