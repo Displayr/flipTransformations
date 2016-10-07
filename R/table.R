75c92882a77898c4ae25034c5d3bfea18fb55528
chschan <chschan@gmail.com>
2016-10-07 14:20:27 +1100

    Added error checking to RemoveRowsAndOrColumns

--- R/table.R
+++ R/table.R
@@ -4,10 +4,12 @@
 #' @param x The data that is being analyzed
 #' @param row.names.to.remove A vector or comma-separated string containing the row labels to remove, if row names are defined. Trailing spaces are removed and lower/upper case is ignored
 #' @param column.names.to.remove A vector or comma-separated string containing the column labels to remove, if column names are defined
+#' @param split delimiter to split string on
 #' @export
 RemoveRowsAndOrColumns <- function(x,
                                    row.names.to.remove = c("NET", "Total", "SUM"),
-                                   column.names.to.remove = c("NET", "Total", "SUM"))
+                                   column.names.to.remove = c("NET", "Total", "SUM"),
+                                   split = ",")
 {
     ind <- list(1:nrow(x), 1:ncol(x))
     for (i in 1:2)
@@ -18,10 +20,18 @@ RemoveRowsAndOrColumns <- function(x,
         if (!is.null(tmpname) && !is.null(tmpstring))
         {
             tmpname <- tolower(trimws(tmpname))
-            tmpstring <- unlist(strsplit(tmpstring, split=","))
+
+            # No splitting if empty delimiter is given (vector is expected)
+            if (split != "")
+                tmpstring <- unlist(strsplit(tmpstring, split=split))
+
             tmpstring <- tolower(trimws(tmpstring))
             ind[[i]] <- which(!tmpname %in% tmpstring)
         }
     }
+
+    if (length(ind[[1]]) == 0 || length(ind[[2]]) == 0)
+        stop ("Removing rows/columns gives empty input matrix\n")
+
     x[ind[[1]], ind[[2]], drop = FALSE]
 }
