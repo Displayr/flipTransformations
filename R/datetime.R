30620d32721f82fbb6c6dc93bf946c46dcf2bd5d
JustinCCYap <justinyapcc@gmail.com>
2016-11-14 16:41:53 +1100

    DS-874: added function that parses date times

---
+++ R/datetime.R
@@ -0,0 +1,36 @@
+#' \code{ParseEnteredData}
+#' @param x Character vector to be parsed.
+#' @param us.format Whether to use the US convention for dates.
+#' @importFrom lubridate parse_date_time
+#' @export
+ParseDateTime <- function(x, us.format = TRUE)
+{
+    .parseSingle <- function(txt)
+    {
+        orders <- if (us.format)
+            c("mdYIMSp", "mdYHMS", "mdYIMp", "mdYHM", "mdY")
+        else
+            c("dmYIMSp", "dmYHMS", "dmYIMp", "dmYHM", "dmY")
+
+        orders <- c(orders, "YmdIMSp", "YmdHMS", "YmdIMp", "YmdHM", "Ymd", "Ym", "Y",
+            "YbdIMSp", "YbdHMS", "YbdIMp", "YbdHM", "Ybd", "Yb",
+            "bdYIMSp", "bdYHMS", "bdYIMp", "bdYHM", "bdY")
+
+        dt <- parse_date_time(txt, orders, quiet = TRUE)
+        if (is.na(dt)) # We check this later due to a bug with parsing "mdY" dates
+        {
+            dt <- parse_date_time(txt, c( "dbYIMSp", "dbYHMS", "dbYIMp", "dbYHM", "dbY", "mY"), quiet = TRUE)
+            if (is.na(dt)) # Work around for bug with parsing "bY" and "by" dates
+                parse_date_time(paste("1", txt), c("dbY", "dby"), quiet = TRUE)
+            else
+                dt
+        }
+        else
+            dt
+    }
+
+    result <- unlist(lapply(x, .parseSingle))
+    class(result) <- c("POSIXct", "POSIXt")
+    attr(result, "tzone") <- "UTC"
+    result
+}
