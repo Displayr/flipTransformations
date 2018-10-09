e9bdb89a22bef6aab45e95d12fa48b43b4b21727
jakehoare <jake_hoare@hotmail.com>
2018-10-09 17:13:39 +1100

    OneHot handle single row and colnames.

--- R/asnumeric.R
+++ R/asnumeric.R
@@ -80,7 +80,9 @@ OneHot <- function(data, outcome = NULL)
     for (i in 1:ncol(X))
         if (!is.null(attr(X[, i], "label")))
             colnames(X)[i] <- attr(X[, i], "label")
-    X <- as.matrix(apply(X, 2, as.numeric), ncol = ncol(X))
+    X <- matrix(apply(X, 2, as.numeric),
+                ncol = ncol(X),
+                dimnames = list(NULL, colnames(X)))
 
     # convert outcome variable to numeric vector (encoding from 0 to nlevels(outcome)-1)
     y <- NULL
