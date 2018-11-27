b434941dacd5fcdd95acc649e0bff947fd9c59db
jakehoare <jake_hoare@hotmail.com>
2018-11-27 11:14:40 +1100

    DS-2300 Add categorical.as.binary to AsDataFrame.

--- R/asdataframe.R
+++ R/asdataframe.R
@@ -2,34 +2,45 @@
 #'
 #' @description Converts input data to a numeric \code{\link{data.frame}}.
 #' @param input.data Either a \code{\link{data.frame}}, a \code{\link{list}} of
-#' \code{\link{data.frame}}s and/or \code{\link{vector}}s, or a \code{\link{matrix}}.
+#'   \code{\link{data.frame}}s and/or \code{\link{vector}}s, or a \code{\link{matrix}}.
 #' @param use.names Whether to use names in place of labels, used only for a
-#' \code{\link{list}} of \code{\link{vector}}s.
+#'   \code{\link{list}} of \code{\link{vector}}s.
 #' @param ignore.columns A list of names of columns to ignore. When \code{input.data}
-#' is a \code{\link{matrix}}, rows are also ignored.
+#'   is a \code{\link{matrix}}, rows are also ignored.
+#' @param categorical.as.binary Whether to convert factors to dummy binary variables,
+#'   or else their levels are converted to integers.
 #' @importFrom flipFormat ExtractCommonPrefix Labels Names
 #' @importFrom flipU RemoveAt
 #' @export
-AsDataFrame <- function(input.data, use.names = FALSE, ignore.columns = "")
+AsDataFrame <- function(input.data,
+                        use.names = FALSE,
+                        ignore.columns = "",
+                        categorical.as.binary = FALSE)
 {
-    dat <- if (is.matrix(input.data)) {
-               mat <- RemoveAt(input.data, at = list(ignore.columns, ignore.columns), split = ",")
-        AsNumeric(data.frame(mat, check.names = FALSE))
-    }else if (is.data.frame(input.data) || !any(sapply(input.data, is.data.frame))) {  # coerce list of variables to data.frame
-        input.data <- data.frame(input.data)
-        colnames(input.data) <- Names(input.data)
-        var.dat <- AsNumeric(ProcessQVariables(input.data), binary = FALSE)
-        # Changing names to labels.
-        if (!use.names)
-            names(var.dat) <- ExtractCommonPrefix(Labels(var.dat))$shortened.labels
-        var.dat
+    dat <- if (is.matrix(input.data))
+        {
+            mat <- RemoveAt(input.data, at = list(ignore.columns, ignore.columns), split = ",")
+            AsNumeric(data.frame(mat, check.names = FALSE))
+        }
+        else if (is.data.frame(input.data) || !any(sapply(input.data, is.data.frame))) # coerce list of variables to data.frame
+        {
+            input.data <- data.frame(input.data)
+            colnames(input.data) <- Names(input.data)
+            var.dat <- AsNumeric(ProcessQVariables(input.data), binary = categorical.as.binary)
+            # Changing names to labels.
+            if (!use.names)
+                names(var.dat) <- ExtractCommonPrefix(Labels(var.dat))$shortened.labels
+            var.dat
 
-    } else if (is.list(input.data)) {       # list of questions and potentially variables
-        names.to.remove <- trimws(unlist(strsplit(ignore.columns, split = ",")))
-        qns.dat <- AsNumeric(QuestionListToDataFrame(input.data, names.to.remove = names.to.remove), binary = FALSE)
-
-    } else
-        stop("input.data must be a data.frame, list of data.frames/vectors or a matrix")
+        }
+        else if (is.list(input.data)) # list of questions and potentially variables
+        {
+            names.to.remove <- trimws(unlist(strsplit(ignore.columns, split = ",")))
+            qns.dat <- AsNumeric(QuestionListToDataFrame(input.data, names.to.remove = names.to.remove),
+                                 binary = categorical.as.binary)
+        }
+        else
+            stop("input.data must be a data.frame, list of data.frames/vectors or a matrix")
 
     return(dat)
 }
--- tests/testthat/test-asdataframe.R
+++ tests/testthat/test-asdataframe.R
@@ -6,6 +6,8 @@ colnames(mat) <- letters[1:20]
 rownames(mat) <- c("where", "is", "the", "love", "at")
 
 df <- data.frame(matrix(seq(1:32), nrow = 4, ncol = 8))
+df2 <- cbind(df, x9 = as.factor(LETTERS[1:4]))
+
 v1 <- c(seq(101, 104))
 v2 <- c(seq(1001, 1004))
 v3 <- c(seq(10001, 10004))
@@ -19,4 +21,7 @@ test_that("AsDataFrame",
     expect_equal(ncol(AsDataFrame(mat, ignore.columns = "k, p, the")), 18)
     expect_equal(ncol(AsDataFrame(l1)), 10)
     expect_equal(ncol(AsDataFrame(l2)), 3)
+
+    expect_equal(ncol(AsDataFrame(df2)), 9)
+    expect_equal(ncol(AsDataFrame(df2, categorical.as.binary = TRUE)), 12)
 })
